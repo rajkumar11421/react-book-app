65dd2dda160760c4d1c25b6ebcfaeb0bbb627314
akuma189 <raj_adlagattu@optum.com>
2023-12-19 17:28:05 +0530

    Working build

--- src/services/BookService.js
+++ src/services/BookService.js
@@ -1,28 +1,27 @@
 import axios from 'axios'
 
-const BOOK_BASE_REST_API_URL = 'http://localhost:8080/api/v1/books';
+const BOOK_BASE_REST_API_URL = 'http://localhost:8080';
 
 class BookService{
 
     getAllBooks(){
-        return axios.get(BOOK_BASE_REST_API_URL)
+        return axios.get(BOOK_BASE_REST_API_URL+'/getBooks')
     }
 
     createBook(book){
-        return axios.post(BOOK_BASE_REST_API_URL, book)
+        return axios.post(BOOK_BASE_REST_API_URL+'/createBooks', book)
     }
 
     getBookById(bookId){
-        return axios.get(BOOK_BASE_REST_API_URL + '/' + bookId);
+        return axios.get(BOOK_BASE_REST_API_URL+'/getBook' + '/' + bookId);
     }
 
     updateBook(bookId, book){
-        return axios.put(BOOK_BASE_REST_API_URL + '/' +bookId, book);
+        return axios.put(BOOK_BASE_REST_API_URL + '/updateBook/' +bookId, book);
     }
 
     deleteBook(bookId){
-        return axios.delete(BOOK_BASE_REST_API_URL + '/' + bookId);
+        return axios.delete(BOOK_BASE_REST_API_URL + '/deleteBook/' + bookId);
     }
 }
-
 export default new BookService();
